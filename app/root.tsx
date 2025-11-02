6a434cc43eae51c14641ea6b252635f6fc535e19
Robin Vitré <robin.vitre@gmail.com>
2025-11-02 23:08:39 +0100

    Vercel Insights

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@
   Scripts,
   ScrollRestoration,
 } from "react-router";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 import type { Route } from "./+types/root";
 import "./app.css";
@@ -37,6 +38,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {children}
         <ScrollRestoration />
         <Scripts />
+        <SpeedInsights />
       </body>
     </html>
   );
